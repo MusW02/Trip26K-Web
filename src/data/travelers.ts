27a11e824fb1846b3efff1f3360c10ea282d8670
Mustafa Waqar <mustafa.waqar02@gmail.com>
2025-12-21 07:45:55 +0500

    Add email addresses for travelers

--- src/data/travelers.ts
+++ src/data/travelers.ts
@@ -10,47 +10,48 @@ export const travelers = [
       name: "Bilal", 
       role: "Bilal-Asif Lord/Party/Daku", 
       image: "/Trip26K-Web/images/bilal.jpeg",
-      email: "" 
+      email: "k224114@nu.edu.pk" 
     },
     { 
       name: "Rayan", 
       role: "King/God", 
       image: "/Trip26K-Web/images/rayan.png",
-      email: "" 
+      email: "k224102@nu.edu.pk" 
     },
     { 
       name: "Sheharyar", 
       role: "Attitude Boy", 
       image: "/Trip26K-Web/images/sher.JPG" ,
-      email: ""
+      email: "k224085@nu.edu.pk"
     },
     { 
       name: "Arman", 
       role: "Lassi/Goga/Dairy Milk/Lahori/Off", 
       image: "/Trip26K-Web/images/lassi.png", 
-      email: ""
+      email: "k228708@nu.edu.pk"
     },
     { 
       name: "Anas", 
       role: "Nalki/Shorty/Chotu/Kid", 
       image: "/Trip26K-Web/images/nalki.png" ,
-      email: ""
+      email: "k224049@nu.edu.pk"
     },
     { 
       name: "Mujtaba", 
       role: "Downie/Fake ACL", 
       image: "/Trip26K-Web/images/downie.png" ,
+      email: "k228709@nu.edu.pk"
     },
     { 
       name: "Aman", 
       role: "Abba Nahi Manay Ge", 
       image: "/Trip26K-Web/images/aman.jpeg" ,
-      email: ""
+      email: "k228714@nu.edu.pk"
     },
     { 
       name: "Ishaq", 
       role: "Dollar/Nalla", 
       image: "/Trip26K-Web/images/dollar.jpeg" ,
-      email: ""
+      email: "k224066@nu.edu.pk"
     },
 ];
