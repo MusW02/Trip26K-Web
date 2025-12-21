5e780858b19de9474da98f3f4b4e2f7e14a4e6ae
Mustafa Waqar <mustafa.waqar02@gmail.com>
2025-12-21 07:18:30 +0500

    Switched to HashRouter to fix 404

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Itinerary from "./pages/Itinerary";
 import Costs from "./pages/Costs";
@@ -16,15 +16,16 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/Trip26K-Web">
+      {/* 👇 Switch to HashRouter and REMOVE the basename */}
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/itinerary" element={<Itinerary />} />
           <Route path="/costs" element={<Costs />} />
-          <Route path="*" element={<NotFound />} />
           <Route path="/admin" element={<Admin />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
